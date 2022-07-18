76cd5a99b341621fef3abe195478cd8338de9963
Anuj Gupta <anujrock9090@gmail.com>
2022-07-19 00:44:30 +0530

    few changes

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ var upload=multer({
 })
 
 app.use(express.static(path.join(__dirname,"public")));
-router.get('/ㅤost',(req,res)=>{
+router.get('/posts/@nuj',(req,res)=>{
     res.render('articles/new',{article:new Article()})
 })
 router.get('/',async (req,res)=>{
